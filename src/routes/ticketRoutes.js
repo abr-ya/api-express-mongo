4abe5a997e2efe4bb32b1737aff3959a7009e17d
abr-ya <gamer05@list.ru>
2022-11-06 13:00:44 +0300

    feature: Add Notes Create and Get

--- src/routes/ticketRoutes.js
+++ src/routes/ticketRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const router = express.Router();
-const { getTickets, getTicket, createTicket, deleteTicket, updateTicket } = require("../controllers/ticketController");
 
+const { getTickets, getTicket, createTicket, deleteTicket, updateTicket } = require("../controllers/ticketController");
 const { protect } = require("../middleware/authMiddleware");
+const noteRouter = require("./noteRoutes");
+
+// Re-route into note router
+router.use("/:ticketId/notes", noteRouter);
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
 router.route("/:id").get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket);
